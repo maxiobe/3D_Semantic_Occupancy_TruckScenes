d461ae8934deda2de8b45eff978a50069cfcd8af
maxiobe <maxi.oberle@web.de>
2025-09-18 11:59:53 +0200

    Changed top k values for truckscenes

--- prediction/SparseOcc/configs/r50_nuimg_704x256_8f.py
+++ prediction/SparseOcc/configs/r50_nuimg_704x256_8f.py
@@ -52,9 +52,10 @@ _dim_ = 256
 _num_points_ = 4
 _num_groups_ = 4
 _num_layers_ = 2
-_num_frames_ = 8
+_num_frames_ = 1 #8
 _num_queries_ = 100
-_topk_training_ = [4000, 16000, 64000]
+#_topk_training_ = [4000, 16000, 64000]
+_topk_training_ = [12000, 32000, 80000]
 _topk_testing_ = [2000, 8000, 32000]
 
 model = dict(
