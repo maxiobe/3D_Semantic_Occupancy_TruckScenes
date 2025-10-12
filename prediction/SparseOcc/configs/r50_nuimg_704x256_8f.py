666b4acc0ee0030d2ff858ca292f7ca3cb4d00fe
maxiobe <maxi.oberle@web.de>
2025-10-12 15:26:03 +0200

    Test with much lower top-k

--- prediction/SparseOcc/configs/r50_nuimg_704x256_8f.py
+++ prediction/SparseOcc/configs/r50_nuimg_704x256_8f.py
@@ -56,7 +56,7 @@ _num_frames_ = 1 #8
 _num_queries_ = 100
 #_topk_training_ = [4000, 16000, 64000]
 #_topk_training_ = [12000, 32000, 80000]
-_topk_training_ = [5000, 2000, 8000]
+_topk_training_ = [500, 2000, 8000]
 _topk_testing_ = [2000, 8000, 32000]
 
 model = dict(
