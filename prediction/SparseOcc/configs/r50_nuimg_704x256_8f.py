0dd5755aa4c5b59bcfb33e7ed7221c2f2511f4ed
maxiobe <maxi.oberle@web.de>
2025-10-12 16:33:04 +0200

    Changed top k value

--- prediction/SparseOcc/configs/r50_nuimg_704x256_8f.py
+++ prediction/SparseOcc/configs/r50_nuimg_704x256_8f.py
@@ -56,7 +56,7 @@ _num_frames_ = 1 #8
 _num_queries_ = 100
 #_topk_training_ = [4000, 16000, 64000]
 #_topk_training_ = [12000, 32000, 80000]
-_topk_training_ = [100, 400, 1600, 6400]
+_topk_training_ = [100, 8400, 160000, 6400]
 _topk_testing_ = [2000, 8000, 32000, 64000]
 
 model = dict(
