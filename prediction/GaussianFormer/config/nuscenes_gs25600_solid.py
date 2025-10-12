65c7039f5f685b38fbf9fdc24c1e53e8d300b45a
maxiobe <maxi.oberle@web.de>
2025-10-12 14:01:07 +0200

    Increased scale of Gaussians

--- prediction/GaussianFormer/config/nuscenes_gs25600_solid.py
+++ prediction/GaussianFormer/config/nuscenes_gs25600_solid.py
@@ -200,7 +200,7 @@ model = dict(
         with_empty=True,
         cuda_kwargs=dict(
             _delete_=True,
-            scale_multiplier=3,
+            scale_multiplier=4, #3
             H=voxel_shape[0], W=voxel_shape[1], D=voxel_shape[2],
             pc_min=pc_range[:3],
             grid_size=grid_size,),
